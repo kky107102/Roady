23608d59b104d516b157963def6098ee11b47d4c
yoojung <yyj07081@gmail.com>
2026-07-28 13:42:47 +0900

    refactor(frontend): 로그인 화면 컴포넌트 구조 정리

--- Roady_FE/e2e/vue.spec.ts
+++ Roady_FE/e2e/vue.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
+test('redirects an unauthenticated user to the login page', async ({ page }) => {
   await page.goto('/')
-  await expect(page.locator('h1')).toHaveText('You did it!')
+
+  await expect(page).toHaveURL(/\/login/)
+  await expect(page.getByRole('heading', { name: '로그인' })).toBeVisible()
 })
