22e6ee76f9022b79ce66bdd139a3fa8f9cf0f640
김경연 <kky107102@naver.com>
2026-08-05 09:23:41 +0900

    refactor(hardware): 카메라 위치 변경 노드명 변경

--- Roady_Embedded/src/hardware/setup.py
+++ Roady_Embedded/src/hardware/setup.py
@@ -18,10 +18,10 @@ setup(
     license="TODO",
     entry_points={
         "console_scripts": [
-            "obstacle_camera_node = hardware.nodes.obstacle_camera_node:main",
+            "tactile_camera_node = hardware.nodes.tactile_camera_node:main",
+            "tactile_camera_capture_viewer = hardware.nodes.tactile_camera_capture_viewer:main",
             "obstacle_camera_capture_viewer = hardware.nodes.obstacle_camera_capture_viewer:main",
-            "wide_camera_node = hardware.nodes.camera_node:main",
-            "wide_camera_capture_viewer = hardware.nodes.wide_camera_capture_viewer:main",
+            "obstacle_camera_node = hardware.nodes.obstacle_camera_node:main",
             "dot_matrix_node = hardware.nodes.dot_matrix_node:main",
             "push_lock_node = hardware.nodes.push_lock_node:main",
             "battery_led_node = hardware.nodes.battery_led_node:main",
